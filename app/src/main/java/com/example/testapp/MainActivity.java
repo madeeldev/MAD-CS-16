1e5bf7f57b450b11d7cec92a2b4de1c1ba9292cf
Adeel <ranaadeelofficial@gmail.com>
2019-10-05 10:16:56 -0700

    activities for message, call and email added
    activities for message, call and email added

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.testapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void SendEmail(View view){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void SendMessage(View view){
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+    }
+    public void Call(View view){
+        Intent intent = new Intent(this, Main4Activity.class);
+        startActivity(intent);
     }
 }
